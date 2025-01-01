3cdadbe60a682b57ed79a38f5afc0ba759cd4653
BaoNgoc <baongochuynh.hbn@gmail.com>
2025-01-01 21:48:12 +0100

    add infos_list_widget

---
+++ lib/components/infos_list_widget.dart
@@ -0,0 +1,22 @@
+import 'package:comics_app/components/infos_widget.dart';
+import 'package:flutter/material.dart';
+
+class InfosListWidget extends StatelessWidget {
+  final List<InfosRow> infos;
+
+  const InfosListWidget({
+    super.key,
+    required this.infos,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: infos,
+      ),
+    );
+  }
+}
