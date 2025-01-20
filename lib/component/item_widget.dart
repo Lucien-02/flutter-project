a9e73ca7bf3199d70f6a0436c55c31c30893c4ac
bhuynh <baongochuynh.hbn@gmail.com>
2025-01-20 23:00:57 +0100

    item widget detail
    (cherry picked from commit 8312b45e3b2ca7d2641e5fc8f7036fe1e016dabe)

--- lib/component/item_widget.dart
+++ lib/component/item_widget.dart
@@ -1,104 +1,136 @@
 import 'package:comics_app/theme/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class ItemWidget extends StatelessWidget {
   final String imageUrl;
   final String title;
   final String? subtitle;
-  const ItemWidget({super.key, required this.imageUrl, required this.title, this.subtitle});
+  final String url;
+  final String type;
+  final int id;
+  const ItemWidget(
+      {super.key,
+      required this.imageUrl,
+      required this.title,
+      this.subtitle,
+      required this.id,
+      required this.type,
+      required this.url});
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      /*onTap: () {
+      onTap: () {
         Widget targetScreen;
         String route_name;
         Object params;
         switch (type) {
           case 'serie':
             route_name = '/serie-detail';
-            params = { "title":name,"imageUrl":imageUrl,"url":url,"id":id};
+            params = {
+              "title": title,
+              "imageUrl": imageUrl,
+              "url": url,
+              "id": id
+            };
             break;
           case 'comic':
             route_name = '/comic-detail';
-            params = { "title":name,"imageUrl":imageUrl,"url":url,"id":id};
+            params = {
+              "title": title,
+              "imageUrl": imageUrl,
+              "url": url,
+              "id": id
+            };
             break;
           case 'film':
             route_name = '/film-detail';
-            params = { "title":name,"imageUrl":imageUrl,"url":url,"id":id};
+            params = {
+              "title": title,
+              "imageUrl": imageUrl,
+              "url": url,
+              "id": id
+            };
             break;
           default:
             route_name = '/serie-detail';
-            params = { "title":name,"imageUrl":imageUrl,"url":url,"id":id};
+            params = {
+              "title": title,
+              "imageUrl": imageUrl,
+              "url": url,
+              "id": id
+            };
         }
-        GoRouter.of(context).push(route_name,extra: params);
-      },*/
+        GoRouter.of(context).push(route_name, extra: params);
+      },
       child: Container(
-            width: 180,
-            height: 242,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8),
-              color: AppColors.cardElementBackground,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // Image
-                ClipRRect(
-                  borderRadius: const BorderRadius.vertical(top: Radius.circular(8)),
-                  child: Image.network(
-                    imageUrl,
+        width: 180,
+        height: 242,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: AppColors.cardElementBackground,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Image
+            ClipRRect(
+              borderRadius:
+                  const BorderRadius.vertical(top: Radius.circular(8)),
+              child: Image.network(
+                imageUrl,
+                width: 180,
+                height: 177,
+                fit: BoxFit.cover,
+                errorBuilder: (context, error, stackTrace) {
+                  return Container(
                     width: 180,
                     height: 177,
-                    fit: BoxFit.cover,
-                    errorBuilder: (context, error, stackTrace) {
-                      return Container(
-                        width: 180,
-                        height: 177,
-                        color: Colors.grey.shade300,
-                        child: const Icon(Icons.broken_image, color: Colors.grey),
-                      );
-                    },
+                    color: Colors.grey.shade300,
+                    child: const Icon(Icons.broken_image, color: Colors.grey),
+                  );
+                },
+              ),
+            ),
+            // Title and Subtitle
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 5),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Title
+                  Text(
+                    title,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                // Title and Subtitle
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 5),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      // Title
-                      Text(
-                        title,
+                  // Subtitle (affiché uniquement si présent)
+                  if (subtitle != null) ...[
+                    const SizedBox(height: 4),
+                    SizedBox(
+                      width: 180,
+                      child: Text(
+                        subtitle!,
                         maxLines: 1,
                         overflow: TextOverflow.ellipsis,
                         style: const TextStyle(
                           fontSize: 16,
-                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
                         ),
                       ),
-                      // Subtitle (affiché uniquement si présent)
-                      if (subtitle != null) ...[
-                        const SizedBox(height: 4),
-                        SizedBox(
-                          width: 180,
-                          child: Text(
-                          subtitle!,
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                          style: const TextStyle(
-                            fontSize: 16,
-                            color: Colors.white,
-                          ),
-                        ),)
-
-                      ],
-                    ],
-                  ),
-                ),
-              ],
+                    )
+                  ],
+                ],
+              ),
             ),
-          ),
+          ],
+        ),
+      ),
     );
   }
 }
